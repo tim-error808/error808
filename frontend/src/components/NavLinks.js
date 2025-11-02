0efcea5bd9063a349dbf5c18be85bc3b3528dd11
Mihael <mihael@exapmle.com>
2025-11-02 18:31:39 +0100

    Dodan gumb za logout

--- frontend/src/components/NavLinks.js
+++ frontend/src/components/NavLinks.js
@@ -5,6 +5,7 @@ const NavLinks = () => {
     <div className="nav-links">
       <Link to="/about">About</Link>
       <Link to="/login">Log In</Link>
+      <Link to="/login">Log out</Link>
     </div>
   );
 };
