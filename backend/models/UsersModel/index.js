a3e817038057fb0e6ed1a0431b530e06b03dd094
markob52 <mb55652@fer.hr>
2025-11-08 15:26:02 +0100

    Inicijalna implementacija UsersModel modela koja je potrebna za implementaciju OAuth2 servera.

---
+++ backend/models/UsersModel/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const {Schema} = require("mongoose");
+
+/**
+ * Mongoose model for the `users` MongoDB collection.
+ *
+ * @constant OAuth2RefreshTokensModel
+ *
+ * @type {Model<Schema>}
+ */
+const UsersModel = mongoose.model("users", new Schema({
+    username: String,
+    passwordHash: String,
+    scope: Array,
+    email: String,
+    role: String,
+    city: String,
+    location: Object,
+    profile: String,
+    createdAt: Date,
+    isActive: Boolean
+}))
+
+module.exports = UsersModel;
