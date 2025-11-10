de53dd73d3ce0f7439b5010a79b1f50b81f8469d
markob52 <mb55652@fer.hr>
2025-11-10 16:09:44 +0100

    Fixing JSDoc of the handleCallback function

--- backend/lib/index.js
+++ backend/lib/index.js
@@ -3,10 +3,10 @@
  *
  * @function handleCallback
  *
- * @param {function():Promise<methodReturnType>}  method                 - method to be run before callback
+ * @param {function():Promise<methodReturnType>}  method                       - method to be run before callback
  * @param {?function(Error,methodReturnType):callbackReturnType} callback=null - callback function
  *
- * @returns {Promise<methodReturnType|undefined>|callbackReturnType}               - callback's return if ran or method's return if not
+ * @returns {Promise<methodReturnType|undefined>|callbackReturnType} - callback's return if ran or method's return if not
  *
  * @template methodReturnType
  * @template callbackReturnType
