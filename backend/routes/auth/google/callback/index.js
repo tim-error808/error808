7e57d31bea05b20deba7c8efbb129d0d0e105e4c
markob52 <mb55652@fer.hr>
2025-11-14 19:24:26 +0100

    Add logging for testing

--- backend/routes/auth/google/callback/index.js
+++ backend/routes/auth/google/callback/index.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 router.get('/', passport.authenticate('google', {
     failureRedirect: 'https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback',
-}, (req, res) => {
-    console.log('User authenticated successfully: ');
+}), (req, res) => {
+    console.log('User authenticated successfully: ', req.user.json);
     res.redirect(`https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback?jwt=${req.user.token}`);
-}))
+});
 
 module.exports = router;
