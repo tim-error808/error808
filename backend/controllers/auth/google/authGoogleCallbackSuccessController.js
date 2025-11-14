504bf2a342cde0e6ccb086868c59df32df3cea27
Marko Bošnjak <141178074+markob52@users.noreply.github.com>
2025-11-14 14:05:09 +0100

    Update authGoogleCallbackSuccessController.js

--- backend/controllers/auth/google/authGoogleCallbackSuccessController.js
+++ backend/controllers/auth/google/authGoogleCallbackSuccessController.js
@@ -3,7 +3,7 @@ const {secrets: GOOGLE_JWT_SECRET, REST_API_PORT} = require('../../../config');
 
 const authGoogleCallbackSuccessController = async (req, res) => {
     const token = jwt.sign({id: req.user.id},GOOGLE_JWT_SECRET,{expiresIn: "7d"});
-    res.redirect(`localhost:${REST_API_PORT}/auth/callback?jwt=${token}`);
+    res.redirect(`https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback?jwt=${token}`);
 }
 
 module.exports = authGoogleCallbackSuccessController;
