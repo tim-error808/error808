8dafca060e516f507f80d8fd93e4589a27d16e71
Marko Bošnjak <141178074+markob52@users.noreply.github.com>
2025-11-14 12:13:08 +0100

    Update CONSTANTS.js so that it loads the correct URI

--- frontend/src/config/CONSTANTS.js
+++ frontend/src/config/CONSTANTS.js
@@ -1,1 +1,1 @@
-export const REST_API_URI = process.env.REST_API_URI;
+export const REST_API_URI = process.env.REST_API_URI | 'https://error808-backend-ftcqdmg7fqcsf0gp.westeurope-01.azurewebsites.net';
