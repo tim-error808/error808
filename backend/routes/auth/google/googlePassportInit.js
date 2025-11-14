ca3f72ca4a6c0a7a1867dfe75b25ef44937ee743
Marko Bošnjak <141178074+markob52@users.noreply.github.com>
2025-11-14 12:40:13 +0100

    Update googlePassportInit.js

--- backend/routes/auth/google/googlePassportInit.js
+++ backend/routes/auth/google/googlePassportInit.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const {secrets: {GOOGLE_AUTH}} = require("../../../config/");
+const {REST_API_PORT, secrets: {GOOGLE_AUTH}} = require("../../../config/");
 const authGoogleVerifyController = require("../../../controllers/auth/google/authGoogleVerifyController");
 
 const googlePassportInit = () =>{
     const strategy = new GoogleStrategy({
         clientID: GOOGLE_AUTH.CLIENT_ID,
         clientSecret: GOOGLE_AUTH.CLIENT_SECRET,
-        callbackURL: "https://www.google.com/oauth2/redirect/google",
+        callbackURL: `localhost:${REST_API_PORT}/auth/google/callback`,
         scope: ['profile', 'email'],
         state: true,
     },authGoogleVerifyController);
