7d71f63a00dc9e9fc3c226eae4bda089b3890ba0
markob52 <mb55652@fer.hr>
2025-11-14 16:24:56 +0100

    Fixing the passport session

--- backend/routes/auth/google/googlePassportInit.js
+++ backend/routes/auth/google/googlePassportInit.js
@@ -4,17 +4,6 @@ const {REST_API_PORT, secrets: {GOOGLE_AUTH}} = require("../../../config/");
 const authGoogleVerifyController = require("../../../controllers/auth/google/authGoogleVerifyController");
 
 const googlePassportInit = () =>{
-    const strategy = new GoogleStrategy({
-        clientID: GOOGLE_AUTH.CLIENT_ID,
-        clientSecret: GOOGLE_AUTH.CLIENT_SECRET,
-        callbackURL: `https://error808-backend-ftcqdmg7fqcsf0gp.westeurope-01.azurewebsites.net/auth/google/callback`,
-        scope: ['profile', 'email'],
-        state: true,
-    },authGoogleVerifyController);
-    passport.use(strategy);
-    passport.serializeUser((user, callback) => callback(null, user));
-    passport.deserializeUser((user, callback) => callback(null, user));
-    passport.initialize();
 }
 
 module.exports = googlePassportInit;
--- backend/routes/auth/google/index.js
+++ backend/routes/auth/google/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
-const googlePassportInit = require('./googlePassportInit');
-const callbackRouter = require('./callback');
-
-googlePassportInit();
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const {secrets: {GOOGLE_AUTH}} = require("../../../config/");
+const authGoogleVerifyController = require("../../../controllers/auth/google/authGoogleVerifyController");
 
 const router = express.Router();
+
+const callbackRouter = require('./callback');
+
+passport.serializeUser((user, callback) => callback(null, user));
+passport.deserializeUser((user, callback) => callback(null, user));
+const strategy = new GoogleStrategy({
+    clientID: GOOGLE_AUTH.CLIENT_ID,
+    clientSecret: GOOGLE_AUTH.CLIENT_SECRET,
+    callbackURL: `https://error808-backend-ftcqdmg7fqcsf0gp.westeurope-01.azurewebsites.net/auth/google/callback`,
+    scope: ['profile', 'email'],
+    state: true,
+},authGoogleVerifyController);
 
 router.use(cookieSession({
     name: 'google-auth-session',
     keys: ['key1','key2']
 }));
+
+passport.use(strategy);
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.get('/', passport.authenticate('google', {
     scope: ['profile', 'email'],
