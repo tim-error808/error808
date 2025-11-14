b5ce42500882e146990a67c618160fb770a37c57
Marko Bošnjak <141178074+markob52@users.noreply.github.com>
2025-11-14 15:28:25 +0100

    Revert "Fix MongoDB User creation"

--- backend/controllers/auth/google/authGoogleCallbackFailureController.js
+++ backend/controllers/auth/google/authGoogleCallbackFailureController.js
@@ -1,7 +1,7 @@
 const {REST_API_PORT} = require("../../../config");
 
 const googleAuthCallbackFailureController = (req, res) => {
-    console.log("Callback Failure:", req);
+    console.log("Callback Failure:", req.json());
     res.redirect(`https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback`);
 }
 
