c71554f7a756fa243f4936a27f5660bf9bda771f
markob52 <mb55652@fer.hr>
2025-12-13 11:28:25 +0100

    make it work for production

--- backend/routes/auth/google/callback/index.js
+++ backend/routes/auth/google/callback/index.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.get(
   "/",
   passport.authenticate("google", {
-    failureRedirect: "http://localhost:3000/auth/login", //https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback
+    failureRedirect: "https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback"
   }),
   (req, res) => {
-    res.redirect("http://localhost:3000/auth/callback"); //https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback
+    res.redirect("https://proud-smoke-033478b03.3.azurestaticapps.net/auth/callback")
   }
 );
 
