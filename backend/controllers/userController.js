e7c9b202b156607f53ca3db63222fd2d39cdf8f7
markob52 <mb55652@fer.hr>
2025-12-14 12:18:01 +0100

    Refactored the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
+//TODO: document the function
 const userController = (req, res) => {
+  /*Note: the reason why mongo is not called here is that because of it 
+  the login algorithm stops working on the frontend for some reason
+  TODO: determine why that happens*/
   const { email, username, scope } = req.user;
 
   return res.status(200).json({
