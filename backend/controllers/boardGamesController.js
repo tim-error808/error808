09fbce563e9f4aacaecd029cb4cdb78317ea89cc
markob52 <mb55652@fer.hr>
2025-12-14 11:36:17 +0100

    refactored a boardgames controller

--- backend/controllers/boardGamesController.js
+++ backend/controllers/boardGamesController.js
@@ -1,3 +1,4 @@
+//TODO: document the function
 const GamesModel = require("../models/GamesModel");
 
 const BoardGameController = async (req, res) => {
@@ -9,7 +10,7 @@ const BoardGameController = async (req, res) => {
     : [];
   const difficultyMap = { easy: 1, medium: 2, hard: 3 };
   const playersMap = {
-    2: [2],
+    "2": [2],
     "3-4": [3, 4],
     "4+": [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
   };
@@ -18,13 +19,16 @@ const BoardGameController = async (req, res) => {
     .filter((d) => d !== undefined);
   const maxPlayers = filters.flatMap((f) => playersMap[f] || []);
   let query = {};
-  if (difficulty.length > 0) query.difficulty = { $in: difficulty };
-  if (maxPlayers.length > 0) query.maxPlayers = { $in: maxPlayers };
+  if (difficulty.length > 0) {
+    query.difficulty = { $in: difficulty };
+  }
+  if (maxPlayers.length > 0) {
+    query.maxPlayers = { $in: maxPlayers };
+  }
   let games = await GamesModel.find(query).lean();
-  const search = req.query.search;
-  if (search) {
+  if (req.query.search) {
     games = games.filter((game) =>
-      game.name.toLowerCase().includes(search.toLowerCase())
+      game.name.toLowerCase().includes(req.query.search.toLowerCase())
     );
   }
   return res.status(200).json(games);
