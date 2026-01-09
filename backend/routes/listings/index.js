9cab8abca9a63a5eb611e4e71886fd38d95ef3cd
markob52 <mb55652@fer.hr>
2026-01-10 00:29:41 +0100

    Fixed routes and their authentication

--- backend/routes/listings/index.js
+++ backend/routes/listings/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {listingsController, deleteListingController, addListingController, getUsersListingsController} = require("../../controllers/listingsController");
+const verifyToken = require("../../middlewares/verifyToken");
 const router = express.Router();
 router.get('/', listingsController);
-router.delete('/remove:listingId', deleteListingController);
-router.put('/new',addListingController);
-router.put('/my', getUsersListingsController);
+router.delete('/remove/:listingId', verifyToken, deleteListingController);
+router.put('/new',verifyToken, addListingController);
+router.put('/my', verifyToken, getUsersListingsController);
 
 module.exports = router;
