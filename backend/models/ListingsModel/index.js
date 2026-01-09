90167459eeab3ac1da11fba2f1444571d4eed212
markob52 <mb55652@fer.hr>
2026-01-09 23:16:20 +0100

    Changed the listings model

--- backend/models/ListingsModel/index.js
+++ backend/models/ListingsModel/index.js
@@ -12,8 +12,16 @@ const ListingsModel = mongoose.model("listings", new Schema({
     available: {type: Boolean, required: true, default: true},
     condition: {type: String, required: true},
     createdAt: {type: Date, required: true, default: Date.now},
-    description: {type: String},
-    game: {type: Schema.Types.ObjectId, ref: "games", required: true },
+    name: {type: String},
+    genre: {type: String},
+    publisher: {type: String},
+    releaseYear: {type: Number},
+    minPlayers:{type: Number},
+    maxPlayers: {type: Number},
+    playTime: {type: Number},
+    difficulty: {type: Number},
+    imageUrl: String,
+    description: String,
     user: {type: Schema.Types.ObjectId, ref: "users", required: true }
 }))
 
