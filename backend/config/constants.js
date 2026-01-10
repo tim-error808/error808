0ced317640a5db1443a78213d8a2ec2c57943cb4
markob52 <mb55652@fer.hr>
2026-01-10 08:58:01 +0100

    Zbog sigurnosti i preglednosti sam stavio konstante za postavke email-a

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -18,8 +18,14 @@ const LOCAL_TEST = process.env.LOCAL_TEST!=='0' || false;
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "<invalid>";
 
+const EMAIL_SENDER = process.env.EMAIL_SENDER || "tabitha99@ethereal.email";
+
+const EMAIL_SMTP_HOST = process.env.EMAIL_SMTP_HOST || "smtp.ethereal.email";
+
 module.exports = {
     REST_API_PORT, 
     LOCAL_TEST,
-    FRONTEND_URL
+    FRONTEND_URL,
+    EMAIL_SENDER,
+    EMAIL_SMTP_HOST
 };
--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -1,17 +1,18 @@
 const nodemailer = require("nodemailer")
 const ComposeWishlistMail = require("../models/MailModel/wishlistMailTemplate.js")
+const {EMAIL_SENDER, EMAIL_SMTP_HOST, secrets: {EMAIL_PASSWORD}} = require("../config")
 const mailComposition = require("../models/MailModel/mailComposition.js")
 
 const MailContoller = async (mailComposition) => {
 
   console.log(mailComposition)
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: EMAIL_SMTP_HOST,
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "tabitha99@ethereal.email", // generated ethereal user
-      pass: "S29gqSNMdXJ42GUnGA", // generated ethereal password
+      user: EMAIL_SENDER, // generated ethereal user
+      pass: EMAIL_PASSWORD, // generated ethereal password
     },
   });
   
